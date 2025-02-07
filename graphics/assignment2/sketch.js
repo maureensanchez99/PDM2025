c14e944cddaacdb3e127c5034f58fcff092eb1a6
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2025-02-07 08:53:26 -0600

    adjusted drawing within canvas

--- graphics/assignment2/sketch.js
+++ graphics/assignment2/sketch.js
@@ -10,7 +10,7 @@ function setup() {
 }
 
 function draw(){
-  drawPalette(105, 105); // draws color pallette
+  drawPalette(105, 105); // draws color palette
 }
 
 function drawCanvas(){
@@ -20,7 +20,8 @@ function drawCanvas(){
 
 function drawPalette(x, y){
   let colors = ["red", "orange", "yellow", "green", "cyan", "blue", "magenta", "brown", "white", "black"];
-
+  stroke(0);
+  strokeWeight(1);
   for(let i = 0; i < colors.length; i++) {
     fill(colors[i]);
     square(x, y + (i * 30), 20);
@@ -28,13 +29,13 @@ function drawPalette(x, y){
 }
 
 function mousePressed() {
-  let palletteX = 105, palletteY = 105;
+  let paletteX = 105, paletteY = 105;
   let squareSize = 20;
   let colors = ["red", "orange", "yellow", "green", "cyan", "blue", "magenta", "brown", "white", "black"];
 
   for(let i = 0; i < colors.length; i++) {
-    if(mouseX > palletteX && mouseX < palletteX + squareSize && 
-      mouseY > palletteY + (i * 30) && mouseY < palletteY + (i * 30) + squareSize){
+    if(mouseX > paletteX && mouseX < paletteX + squareSize && 
+      mouseY > paletteY + (i * 30) && mouseY < paletteY + (i * 30) + squareSize){
       selectedColor = colors[i];
       return;
     }
@@ -42,8 +43,21 @@ function mousePressed() {
 }
 
 function mouseDragged(){
-  if(mouseX > 100 && mouseX < 800 && mouseY > 100 && mouseY < 700){
+  let canvasLeft = 100, canvasRight = 800;
+  let canvasTop = 100, canvasBottom = 700;
+  let paletteLeft = 105, paletteRight = 125;
+  let paletteTop = 105, paletteBottom = 405;
+
+  let x1 = constrain(mouseX, canvasLeft, canvasRight);
+  let y1 = constrain(mouseY, canvasTop, canvasBottom);
+  let x2 = constrain(pmouseX, canvasLeft, canvasRight);
+  let y2 = constrain(pmouseY, canvasTop, canvasBottom);
+
+  let insidePalette = (mouseX > paletteLeft && mouseX < paletteRight && mouseY > paletteTop && mouseY < paletteBottom);
+
+  if(!insidePalette){
     stroke(selectedColor);
-    line(pmouseX, pmouseY, mouseX, mouseY);
+    strokeWeight(5);
+    line(x2, y2, x1, y1);
   }
 }
