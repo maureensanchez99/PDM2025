77511ff6adb5601ea5492d8b77ae0c8322050b4a
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2025-04-30 00:19:33 -0500

    got graphic working

--- audio/assignment3/sketch.js
+++ audio/assignment3/sketch.js
@@ -1,7 +1,25 @@
+let punchImage, afterImage;
+
+function preload() {
+  punchImage = loadImage('media/punch.png');
+  afterImage = loadImage('media/after.png');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
+  console.log("Setup function is running");
 }
 
 function draw() {
-  background(220);
+  console.log("Draw function is running");
+  background(punchImage);
+
+  // Display the afterImage while the mouse is pressed
+  if (mouseIsPressed) {
+    image(afterImage, 0, 0, 800, 600);
+  }
+}
+
+function mousePressed() {
+  console.log("Mouse pressed");
 }
