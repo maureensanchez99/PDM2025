1ffcfb39a96ce1b7ff6448b9ba089151feb40540
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2025-05-01 20:32:08 -0500

    finished code

---
+++ hardware/assignment1/sketch/sketch.ino
@@ -0,0 +1,116 @@
+/*
+Assignment 1 of Hardware Section  
+You work for the haunted house company 17th Turnstile. In a new scary sci-fi exhibit they want you to detect a person's presence and trigger electrical items to flicker on and off sporadically. Do this with physical computing...  
+
+You will need:   
+- two digital inputs (i.e., two buttons)  
+- two LEDs (or a buzzer or something else you can turn on/off)  
+- code to make it blink in different patterns depending on which button is pressed  
+
+For a challenge, have it say something in morse code.  
+*/
+
+#include <Arduino.h>
+
+const int buttonPin1 = 2, buttonPin2 = 3;  // pushbutton pins
+int buttonPin1State, buttonPin2State;  // pushbutton pins state
+const int ledPin1 = 12, ledPin2 = 13;  // LED pins
+
+
+void setup() {
+    // initalizes buttons to be inputs
+    pinMode(buttonPin1, INPUT);
+    pinMode(buttonPin2, INPUT);
+
+    // initializes LEDs as outputs
+    pinMode(ledPin1, OUTPUT);
+    pinMode(ledPin2, OUTPUT);
+}
+
+void loop() {
+    buttonPin1State = digitalRead(buttonPin1);
+    buttonPin2State = digitalRead(buttonPin2);
+
+    if (buttonPin1State == HIGH){
+        boo();
+    } else if (buttonPin2State == HIGH){
+        getOut();
+    }
+}
+
+void dot(int ledPin) {
+  digitalWrite(ledPin, HIGH);
+  delay(200);
+  digitalWrite(ledPin, LOW);
+  delay(200);
+}
+
+void dash(int ledPin) {
+  digitalWrite(ledPin, HIGH);
+  delay(600);
+  digitalWrite(ledPin, LOW);
+  delay(200);
+}
+
+void letterSpace() {
+  delay(600);
+}
+
+void wordSpace() {
+  delay(1400);
+}
+
+// reads button1 and plays "Boo" in morse code with light
+void boo(){
+  // B: - . . .
+  dash(ledPin1);
+  dot(ledPin1);
+  dot(ledPin1);
+  dot(ledPin1);
+  letterSpace();
+
+  // O: - - -
+  dash(ledPin2);
+  dash(ledPin2);
+  dash(ledPin2);
+  letterSpace();
+
+  // O: - - -
+  dash(ledPin1);
+  dash(ledPin1);
+  dash(ledPin1);
+  letterSpace();
+}
+
+// reads button2 and plays "Get Out" in morse code with light
+void getOut(){
+      // G: --.
+  dash(ledPin1);
+  dash(ledPin1);
+  dot(ledPin1);
+  letterSpace();
+
+  // E: .
+  dot(ledPin2);
+  letterSpace();
+
+  // T: -
+  dash(ledPin1);
+  wordSpace();
+
+  // O: ---
+  dash(ledPin2);
+  dash(ledPin2);
+  dash(ledPin2);
+  letterSpace();
+
+  // U: ..-
+  dot(ledPin1);
+  dot(ledPin1);
+  dash(ledPin1);
+  letterSpace();
+
+  // T: -
+  dash(ledPin2);
+  wordSpace();
+}
