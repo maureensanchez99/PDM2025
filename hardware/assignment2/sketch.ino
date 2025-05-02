a1909e57715999248928368e7ef5a30a40b4a9c4
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2025-05-01 21:58:32 -0500

    started assignment 2

---
+++ hardware/assignment2/sketch.ino
@@ -0,0 +1,20 @@
+/*
+Assignment 2 of Hardware Section  
+PWM Game  
+
+Create a two-person game using at least one analog input (Photoresistor, pressure sensor, flex sensor, etc. typically in a resistor-divider network) and at least one pulse-width-modulated (Analog) output (buzzer, LED, etc).     
+
+For instance, you could create a circuit that has a user press a pressure sensor causing an LED to become steadily righter and then stays at it's brightest point. Then the next person tries to make another light go brighter. Perhaps if they go too far, it reverses direction and dims.  
+
+You must include at least one sentence describing each of the following components in the comments: game objective, rules, challenge, and interaction.  
+*/
+
+#include <Arduino.h>
+
+void setup() {
+   
+}
+
+void loop() {
+  
+}
