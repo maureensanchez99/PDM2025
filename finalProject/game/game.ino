ce3113c1b3f8ffa83ceac617b320d88f07606833
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2025-05-05 03:07:01 -0500

    started working on hardware side

--- finalProject/game/game.ino
+++ finalProject/game/game.ino
@@ -1,7 +1,35 @@
-void setup (){
+const int ledPin = 9;
+const int buttonPin = 2;
+const int buzzerPin = 8;
 
+int buttonState;
+
+void setup (){
+  Serial.begin(9600);
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop (){
+  buttonState = digitalRead(buttonPin);
 
+  playThunder();
+
+  digitalWrite(ledPin, HIGH);
+}
+
+void playThunder() {
+  long duration = random(500, 1500); // Duration of thunder
+  long startTime = millis();
+
+  while (millis() - startTime < duration) {
+    int freq = random(60, 200); // Low rumbling frequency range
+    int dur = random(20, 100);  // Short burst
+
+    tone(buzzerPin, freq, dur);
+    delay(dur + random(10, 50));
+  }
+
+  noTone(buzzerPin);
 }
--- finalProject/sketch.js
+++ finalProject/sketch.js
@@ -1,7 +1,35 @@
+let port;
+let connectButton;
+let backgroundColor;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+  colorMode(HSB, 255);
+
+  port = createSerial();
+  connectButton = createButton("Connect to Arduino");
+  connectButton.mousePressed(connectToSerial);
 }
 
 function draw() {
-  background(220);
+  background(backgroundColor);
+
+  if (port.opened()) {
+    let msg = `${r} ${g} ${b}\n`;
+    port.write(msg);
+  }
+
+  // Read light sensor value from Arduino
+  let str = port.readUntil('\n');
+  if (str !== "") {
+    let lightVal = Number(str.trim());
+    if (!isNaN(lightVal)) {
+      let hue = map(val, 0, 132, 0, 360);
+      backgroundColor = color(hue, 255, 255);
+    }
+  }
+}
+
+function connectToSerial() {
+  port.open('Arduino', 9600);
 }
